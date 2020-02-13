1d83e0fe5e085323c941505340151364dfd90643
James Martin <jamesmartin@github.com>
2020-02-14 09:44:31 +1100

    Remove ActiveRecord to make tests easier

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.active_support.test_order = :random
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -13,7 +15,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  #config.serve_static_assets  = true
+  config.serve_static_files = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
@@ -29,7 +32,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
